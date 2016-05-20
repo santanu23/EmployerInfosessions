2ea70115dcee63be7c3501fbfe4fd76b9681d75d
Santanu Sarker <sarkersantanu@gmail.com>
2016-05-20 17:32:09 -0400

    greyout past infosessions + centered ul

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         var infosessions = [];
         var tomorrow = [];
         $(data.data).each(function(index, element) {
-            if (element.date == moment().format("YYYY-MM-DD")) infosessions.push(element);
+            if (element.date == moment().add(0, 'days').format("YYYY-MM-DD")) infosessions.push(element);
             if (element.date == moment().add(1, 'days').format("YYYY-MM-DD")) tomorrow.push(element.employer);
         });
         infosessions.sort(function(a, b) {
@@ -25,24 +25,28 @@ $(document).ready(function() {
         console.log(tomorrow);
         if (infosessions.length > 0) {
             $(infosessions).each(function(index, element) {
-                if (element.employer.indexOf("* CANCELLED *") == -1) {
+                if (element.employer.indexOf("* CANCELLED *") == -1) { //don't include cancelded info sessions
                     var programs = getProgramFromAudience(element.audience);
                     insertCard(element.employer, "images/employers/" + element.employer.toLowerCase().trim().replace(/ /g, '').replace(".", "") + ".jpg", programs, element.start_time, element.end_time,
-                        element.building.code, element.building.room, element.building.map_url, element.link, element.description);
+                        element.building.code, element.building.room, element.building.map_url, element.link, element.description, moment().format("HH:mm") > element.start_time);
                 }
             });
         } else {
             $('.mainContainer').append("<center><h3 class=\"noInfoSessions\">No infosessions today<h3><center>");
         }
-        //calenderStuff();
+        //TODO: calenderStuff();
     });
 });
 
-var insertCard = function(employerName, imageSrc, programList, start, end, buildingCode, buildingRoom, mapUrl, registerUrl, description) {
-var cardTemplate = "<div class=\"infosessionCard\">"
-+                      "<div class=\"card\">"
+var insertCard = function(employerName, imageSrc, programList, start, end, buildingCode, buildingRoom, mapUrl, registerUrl, description, pastEvent) {
+var cardTemplate;
+if (pastEvent) {
+    cardTemplate = "<div class=\"infosessionCard greyOut\">";
+} else {
+    cardTemplate = "<div class=\"infosessionCard\">";
+}
+cardTemplate +=       "<div class=\"card\">"
 +                          "<div class=\"card-image\">"
-                               //add first image from google images by searching for emplyoer name
 +                              "<img class=\"logo\" src=\"" + imageSrc + "\">"
 +                          "</div>"
 +                          "<div class=\"card-content\">"
@@ -51,7 +55,6 @@ var cardTemplate = "<div class=\"infosessionCard\">"
 +                              "<p class = \"Location\"><a href=\""+ mapUrl + "\"><i class=\"fa fa-map-marker \" aria-hidden=\"true\"></i>  " + buildingCode + " " + buildingRoom + "</a></p>";
 //add chips for each program (eg. Eng-Comp, Eng-Soft, Math-CS)
  $(programList).each( function(index, element){
-   //if (index % 3 == 0) cardTemplate += "<br>";
    cardTemplate += "<div class=\"program chip\">" + element + "</div>";
  });
 cardTemplate +=            "</div>"
